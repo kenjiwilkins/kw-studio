be9cc99a3164b647130c95c084460812f24fddf4
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-07-18 23:38:07 +0900

    modify: Icon List

--- src/components/common/IconList.tsx
+++ src/components/common/IconList.tsx
@@ -14,12 +14,27 @@ type Props = {
     title: string;
     emoji?: string;
     id: string;
-  };
+  }[];
   onClick?: (id: string) => void;
 };
 
 export const IconList = (props: Props) => {
   return (
-    <List subheader={<ListSubheader>{props.listTitle}</ListSubheader>}></List>
+    <List subheader={<ListSubheader>{props.listTitle}</ListSubheader>}>
+      {props.items.map((item, index) => (
+        <ListItem key={index}>
+          <ListItemButton onClick={() => props.onClick?.(item.id)}>
+            <ListItemIcon>
+              {item.imageUrl ? (
+                <img src={item.imageUrl} alt={item.title} />
+              ) : (
+                item.emoji
+              )}
+            </ListItemIcon>
+            <ListItemText primary={item.title} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
   );
 };
